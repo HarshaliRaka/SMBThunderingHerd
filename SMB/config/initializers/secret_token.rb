f22c0857969e074cbb2343dae51c5bd75e502f51
harshali <harshali@harshali.(none)>
2015-03-29 12:50:30 +0530

    new jruby project intial struct

---
+++ SMB/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SMB::Application.config.secret_token = '577a6a1ba9c8feaadcf5977869b728727667b17adcd1777547117acc2bc8826c312bd7d2efae6ba4d4a821b962683e826175504a153aecc7435c21d419670056'
