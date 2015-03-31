805d1a48fe888cd31a6b9eb6e3acd5807367c2b1
shiksingh <shiksingh@ebay.com>
2015-03-31 20:51:28 +0530

    adding image

--- SMB/app/controllers/top_products_controller.rb
+++ SMB/app/controllers/top_products_controller.rb
@@ -2,7 +2,7 @@ class TopProductsController < ApplicationController
 
   def get_product
     @topProducts = TopProduct.getTop10Products
-    render :top_product
+    render('top_product')
   end
 
 end
--- SMB/app/models/top_product.rb
+++ SMB/app/models/top_product.rb
@@ -5,10 +5,11 @@ class TopProduct
     data = HTTParty.get("http://svcs.ebay.com/MerchandisingService?OPERATION-NAME=getTopSellingProducts&SERVICE-NAME=MerchandisingService&SERVICE-VERSION=1.1.0&X-EBAY-SOA-GLOBAL-ID=EBAY-IN&CONSUMER-ID=YourAppID&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&maxResults=10")
     products =  data["getTopSellingProductsResponse"]["productRecommendations"]["product"]
     @top10Products = []
+
     products.each do |product|
-      @top10Products << {:name => product["title"]}
+      prod = product["title"]
+      @top10Products << prod
     end
-    puts @top10Products
     return @top10Products
   end
 end
